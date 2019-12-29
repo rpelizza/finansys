2d29a0a624cff4ac58e8986ca11ebf9591a1e98f
Rafael <Pelizza>
2019-12-29 15:51:24 -0300

    model e tslint

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,16 +1,18 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api'
+
+import { Category } from './pages/categories/shared/category.model'
 
 export class InMemoryDatabase implements InMemoryDbService {
 
     createDb() {
         
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
             { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios' },
             { id: 3, name: 'Lazer', description: 'Cinema, parques, bares, etc' },
             { id: 4, name: 'Salário', description: 'Recebimento de salário' },
-            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' }
-        ];
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer' },
+        ]
 
         return { categories }
 
--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoriesRoutingModule } from './categories-routing.module'
+import { CategoryListComponent } from './category-list/category-list.component'
+import { CategoryFormComponent } from './category-form/category-form.component'
 
 
 @NgModule({
