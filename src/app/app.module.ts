dfc8c5daf4cf2eb5d9136ec320b32b707086cb7a
Rafael <Pelizza>
2020-01-04 16:28:38 -0300

    remove rollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import * as Rollbar from 'rollbar';
 import { BrowserModule } from '@angular/platform-browser'
-import { Injectable, Injector, InjectionToken, NgModule, ErrorHandler, Inject } from '@angular/core';
+import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http';
 
 import { LoadingBarModule } from '@ngx-loading-bar/core';
@@ -11,27 +10,6 @@ import { AppComponent } from './app.component'
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { InMemoryDatabase } from './in-memory-database'
-
-const rollbarConfig = {
-	accessToken: '64b5691c436a4e3d880f70ad4459628b',
-	captureUncaught: true,
-	captureUnhandledRejections: true,
-};
-
-@Injectable()
-export class RollbarErrorHandler implements ErrorHandler {
-	constructor(@Inject(RollbarService) private rollbar: Rollbar) { }
-
-	handleError(err: any): void {
-		this.rollbar.error(err.originalError || err);
-	}
-}
-
-export function rollbarFactory() {
-	return new Rollbar(rollbarConfig);
-}
-
-export const RollbarService = new InjectionToken<Rollbar>('rollbar');
 
 @NgModule({
 	declarations: [
@@ -46,10 +24,7 @@ export const RollbarService = new InjectionToken<Rollbar>('rollbar');
 
 		HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
 	],
-	providers: [
-		{ provide: ErrorHandler, useClass: RollbarErrorHandler },
-		{ provide: RollbarService, useFactory: rollbarFactory }
-	  ],
+	providers: [],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
