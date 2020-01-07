55dd2f7eb590e532d133ef7145f288c50f73de9a
Rafael <Pelizza>
2020-01-06 23:50:32 -0300

    mudança pasta sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component'
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { InMemoryDatabase } from './in-memory-database';
-import { ErrorHandlerService } from './core/errorHandler/error-handler.service';
+import { ErrorHandlerService } from './core/service/error-handler/error-handler.service';
 
 @NgModule({
 	declarations: [AppComponent],
