bdf05c0b3199fecfed4740868a7286a8b846d762
Rafael <Pelizza>
2020-01-16 19:39:04 -0300

    setting up EntriesModule

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 import { Observable, throwError } from 'rxjs'
-import { map, catchError, flatMap } from 'rxjs/operators'
+import { map, catchError } from 'rxjs/operators'
 
 import { Category } from './category.model'
 
