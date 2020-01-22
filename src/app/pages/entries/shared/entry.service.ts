41debcf537b4207f93b92f63266758da6a4230e4
Rafael <Pelizza>
2020-01-22 16:22:35 -0300

    melhorando entry service com baseresouceservice

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,15 +1,53 @@
 import { Injectable, Injector } from '@angular/core'
 
+import { Observable } from 'rxjs'
+import { flatMap } from 'rxjs/operators'
+
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service'
 import { Entry } from './entry.model'
+import { CategoryService } from '../../categories/shared/category.service'
 
 @Injectable({
 	providedIn: 'root'
 })
 export class EntryService extends BaseResourceService<Entry> {
 
-	constructor(protected injector: Injector) {
+	constructor(protected injector: Injector, private categoryService: CategoryService) {
 		super('api/entries', injector)
 	}
+
+	create(entry: Entry): Observable<Entry> {
+		return this.categoryService.getById(entry.categoryId).pipe(
+			flatMap(category => {
+				entry.category = category				
+				return super.create(entry)
+		}))
+	}
+
+	update(entry: Entry): Observable<Entry> {
+		
+		return this.categoryService.getById(entry.categoryId).pipe(
+			flatMap(category => {
+				entry.category = category
+				return super.update(entry)
+			})
+		)
+	}
+
+	// ! Protected Methods
+	protected jsonDataToResources(jsonData: any[]): Entry[] {
+		const entries: Entry[] = []
+
+		jsonData.forEach(el => {
+			const entry = Object.assign(new Entry(), el)
+			entries.push(entry)
+		})
+
+		return entries
+	}
+
+	protected jsonDataToResource(jsonData: any): Entry {
+		return Object.assign(new Entry(), jsonData)
+	}	
 
 }
