cbf1cc0332f95006f33457656eafac0ee04d6a92
Rafael <Pelizza>
2020-01-24 21:18:09 -0300

    BaseResourceListComponent criado e melhorando CategoryList e EntryList

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component'
 
 import { Category } from '../shared/category.model'
 import { CategoryService } from '../shared/category.service'
@@ -8,29 +10,12 @@ import { CategoryService } from '../shared/category.service'
 	templateUrl: './category-list.component.html',
 	styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent implements OnInit {
-
-	categories: Category[] = []
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
 	constructor(
 		private categoryService: CategoryService
-	) { }
-
-	ngOnInit() {
-		this.categoryService.getAll().subscribe((res) => {
-			this.categories = res
-		})
-	}
-
-	deleteCategory(category) {
-		const mustDelete = confirm('Deseja realmente excluir este item?')
-		if (mustDelete) {
-			this.categoryService.delete(category.id).subscribe(() => {
-				this.categories = this.categories.filter(element => element !== category)
-			}, (error) => {
-				console.log('Erro ao deletar a categoria')
-			})
-		}
+	) {
+		super(categoryService)
 	}
 
 }
--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component'
 
 import { Entry } from '../shared/entry.model'
 import { EntryService } from '../shared/entry.service'
@@ -8,27 +10,12 @@ import { EntryService } from '../shared/entry.service'
 	templateUrl: './entry-list.component.html',
 	styleUrls: ['./entry-list.component.scss']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-	entries: Entry[] = []
-
-	constructor(private entryService: EntryService) { }
-
-	ngOnInit() {
-		this.entryService.getAll().subscribe((res) => {
-			this.entries = res.sort((a, b) => b.id - a.id)
-		})
-	}
-
-	deleteEntry(entry) {
-		const mustDelete = confirm('Deseja realmente excluir este item?')
-		if (mustDelete) {
-			this.entryService.delete(entry.id).subscribe(() => {
-				this.entries = this.entries.filter(element => element !== entry)
-			}, (error) => {
-				console.log('Erro ao tentar excluir')
-			})
-		}
+	constructor(
+		private entryService: EntryService
+	) {
+		super(entryService)
 	}
 
 }
